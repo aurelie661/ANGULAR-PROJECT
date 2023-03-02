364fdd5849b47eb47cd7e370404514f8a3e1b85c
aurelie661 <boutaurel59@gmail.com>
2023-03-02 10:43:50 +0100

    projet pokemon

---
+++ projet pokemon/pokemon/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+     `
+})
+export class AppComponent {
+  title = 'pokemon';
+}
