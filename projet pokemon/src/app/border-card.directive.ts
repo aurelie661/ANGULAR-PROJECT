7117ade2829d203bb2bbf2788c410779e0d31b5e
aurelie661 <boutaurel59@gmail.com>
2023-05-29 01:07:24 +0200

    pipe learning in progress

--- projet pokemon/src/app/border-card.directive.ts
+++ projet pokemon/src/app/border-card.directive.ts
@@ -1,28 +1,29 @@
-import { Directive, ElementRef , HostListener} from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[pokemonBorderCard]'
+  selector: '[pokemonBorderCard]',
 })
 export class BorderCardDirective {
-
-  constructor(private el:ElementRef) {
-    this.setHeight(220);
+  constructor(private el: ElementRef) {
+    this.setHeight(185);
     this.setBorder('#f5f5f5');
   }
 
-    @HostListener('mouseenter') onMouseEnter(){
-      this.setBorder('#009688');
-    }
-    @HostListener('mouseleave') onMouseLeave(){
-      this.setBorder('#f5f5f5');
-    }
+  @Input('pokemonBorderCard') borderColor: string;
 
-    private setHeight(height : number){
-      this.el.nativeElement.style.height = `${height}px`;
-    }
-   
-    private setBorder(color: string){
-      let border = 'solide 4 px' +color;
-      this.el.nativeElement.style.border = border;
-    }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder(this.borderColor || '#009688');
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  }
+
+  private setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  private setBorder(color: string) {
+    let border = 'solid 4px' + color;
+    this.el.nativeElement.style.border = border;
+  }
 }
