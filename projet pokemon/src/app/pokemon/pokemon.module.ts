2776b03a7ac3629974441180eef5b7611c6823cc
aurelie661 <boutaurel59@gmail.com>
2023-06-09 23:09:26 +0200

    setting up authentication

--- projet pokemon/src/app/pokemon/pokemon.module.ts
+++ projet pokemon/src/app/pokemon/pokemon.module.ts
@@ -12,10 +12,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { PokemonService } from './pokemon.service';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { LoaderComponent } from './loader/loader.component';
+import { AuthGuard } from '../auth.guard';
 
 const pokemonRoutes: Routes = [
 
-  {path: 'edit/pokemon/:id', component: EditPokemonComponent},
+  {path: 'edit/pokemon/:id', component: EditPokemonComponent, canActivate: [AuthGuard]},
   {path: 'pokemon/add', component: AddPokemonComponent},
   {path: 'pokemons', component: ListPokemonComponent},
   {path: 'pokemon/:id', component: DetailPokemonComponent},
