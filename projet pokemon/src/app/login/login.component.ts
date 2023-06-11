9ea2858ede7bf19b2e22b059406c5882ef8d5275
aurelie661 <boutaurel59@gmail.com>
2023-06-11 23:06:10 +0200

    finished connection

--- projet pokemon/src/app/login/login.component.ts
+++ projet pokemon/src/app/login/login.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: [],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  message: string = "Vous êtes déconecté ('pikachu'/'pikachu')";
+  name: string;
+  password: string;
+  auth: AuthService;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.auth = this.authService;
+  }
+
+  setMessage() {
+    if (this.auth.isLoggedIn) {
+      this.message = "Vous êtes connectés.";
+    } else {
+      this.message = "Identifiant ou mot de passe incorrect.";
+    }
+  }
+
+  login() {
+    this.message = 'Tentative de connection en cours...';
+    this.auth.login(this.name,this.password)
+    .subscribe((isLoggedIn: boolean)=>{
+      this.setMessage();
+      if(isLoggedIn){
+          this.router.navigate(['/pokemons']);
+      }else{
+        this.password = '';
+        this.router.navigate(['/login']);
+      }
+
+    })
+  }
+
+  logout() {
+    this.authService.logout();
+    this.message = "Vous êtes déconnecté.";
+    this.router.navigate(["/login"]);
+  }
 }
